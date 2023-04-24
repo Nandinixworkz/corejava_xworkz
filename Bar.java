9899b0b34834e3a6dd527e8c5f128f846e3ee3d1
Tejashwini Ganiger <tejashwiniganiger.xworkz@gmail.com>
2023-04-24 13:10:49 +0530

    All arrays files are added

---
+++ Bar.java
@@ -0,0 +1,23 @@
+class Bar{
+       static String[] alcohol = {"Royal Stag","Black Label","Black&White","100 Piper's",
+	   "Budwiser","Tuborg","Signature",
+	   "Teacher's","Amrut","Ballantine","Kingfisher","Old monk","Black Dog","Bira","Brocode"};
+	    
+		public static void main(String nandini[]){
+		
+		//System.out.println("List of brands avaible:");
+       //System.out.println("1:" + alcohol[1] + "\n" + "2:" + alcohol[2] + 
+//"\n" + "3:" + alcohol[3] + "\n" + "4:" + alcohol[4] + "\n" + "5:" + alcohol[5] + "\n"
+	//	+ "6:" + alcohol[6] + "\n" + "7:" + alcohol[7] + "\n" + "8:" + alcohol[8] + "\n" + "9:" 
+		//+ alcohol[9] + "\n" + "10:" + alcohol[10] + "\n"
+	//	+ "11:" + alcohol[11] + "\n" + "12:" + alcohol[12] 
+		//+ "\n" + "13:" + alcohol[13] + "\n" + "14:" + alcohol[14] +  "\n" + "15:" + alcohol[15]);
+		 for(int index = 0; index <= alcohol.length - 1; index++)
+		 {
+			 System.out.println(alcohol[index]);
+		 
+		 }
+		
+		}
+	   
+}
--- SwiggyTester1.java
+++
@@ -1,9 +0,0 @@
-class SwiggyTester1{
-     public static void main(String swiggy[]){
-	 
-	 String itemName = "Baby Corn";
-	 double price = Swiggy.searchItem(itemName);
-	 System.out.println("The price of " +itemName+" is"+price);
-	 System.out.println("Main method ended");
-	 }
-}
